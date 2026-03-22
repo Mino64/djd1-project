ca827aef62f723318b14bc49a7d2bc69b8802237
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-22 17:18:43 +0000

    Daniel - iteration 3 done, changed seesaw and gave boxes and player weight for the seesaw

--- Assets/Scripts/Environment/Seesaw/SmoothSeesaw2D.cs
+++ Assets/Scripts/Environment/Seesaw/SmoothSeesaw2D.cs
@@ -5,7 +5,7 @@ public class SmoothSeesaw2D : MonoBehaviour
 {
     public float maxAngle = 25f;
     public float speed = 5f;
-    public float maxDistance = 3f; // distance from center for full effect
+    public float maxDistance = 3f;
     public float deadZone = 0.2f;
 
     private List<GameObject> allObjects = new List<GameObject>();
@@ -21,17 +21,20 @@ public class SmoothSeesaw2D : MonoBehaviour
 
             float distance = obj.transform.position.x - centerX;
 
-            // ignore tiny center movement
+            
             if (Mathf.Abs(distance) < deadZone)
                 continue;
 
-            // normalize distance (-1 to 1)
+            
             float normalized = Mathf.Clamp(distance / maxDistance, -1f, 1f);
 
-            torque += normalized;
+            Weights w = obj.GetComponent<Weights>();
+            float weight = (w != null) ? w.weight : 1f;
+
+            torque += normalized * weight;
         }
 
-        // convert torque into angle
+        
         float targetAngle = -torque * maxAngle;
 
         Quaternion targetRot = Quaternion.Euler(0, 0, targetAngle);
