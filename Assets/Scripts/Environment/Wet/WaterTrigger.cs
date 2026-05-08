4ba58de724ba4f3109e6d38f57acc56bdfafa1a9
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-05-08 16:26:49 +0000

    Agua a funcionar corretamente nas cores certas

--- Assets/Scripts/Environment/Wet/WaterTrigger.cs
+++ Assets/Scripts/Environment/Wet/WaterTrigger.cs
@@ -1,12 +1,15 @@
+using System;
 using UnityEngine;
 
 public class WaterActivator : MonoBehaviour
 {
     [SerializeField] private RisingWater risingWater;
+    [SerializeField] GameObject objetoAlvo;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Cat"))
             risingWater.StartRising();
+            objetoAlvo.SetActive(true);
     }
 }
