5adc48f78f19058ca07ebcd1fd326ca618b7c018
Kirin <kirinlyraota.work@gmail.com>
2026-05-18 23:30:12 +0100

    changes done to scenes : Settings & MoinhoLVL. Kirin

--- Assets/Scripts/KirinScripts/StopOnTrigger.cs
+++ Assets/Scripts/KirinScripts/StopOnTrigger.cs
@@ -1,16 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
 public class StopOnTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float stopDuration = 5f;
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (hasTriggered) return;
+
+        if (other.CompareTag("Cat"))
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                hasTriggered = true;
+                StartCoroutine(FreezePlayer(player));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FreezePlayer(Player player)
     {
-        
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        Animator animator = player.GetComponent<Animator>();
+
+        if (rb != null) rb.linearVelocity = Vector2.zero;
+
+        if (animator != null)
+        {
+            animator.SetFloat("AbsVelocityX", 0f);
+            animator.SetBool("IsGrounded", true);
+        }
+
+        player.enabled = false;
+
+        yield return new WaitForSeconds(stopDuration);
+
+        player.enabled = true;
     }
 }
