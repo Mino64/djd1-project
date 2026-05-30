4d7a41298ea561bffb9d3f200000e26194b1ff81
Daniel <daniel.henriques.gamedev@gmail.com>
2026-05-31 00:53:11 +0100

    full credits change for optimization

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -4,83 +4,27 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
-    [Header("Scrolling")]
-    [SerializeField] private float scrollSpeed = 80f;
-
-    [Header("LoadScene")]
-    [SerializeField] private int menuSceneNumber = 2;
+    [SerializeField] private float scrollSpeed = 50f;
+    [SerializeField] private float endYPosition = 2000f;
+    [SerializeField] private int menuSceneNumber;
     [SerializeField] private float tempomenu = 2f;
-    [SerializeField] Animator animacao;
-
-    private RectTransform rectTransform;
-    private Camera uiCamera;
-    private bool canScroll = false;
-    private float endWorldY;
-
-    private void Awake()
-    {
-        rectTransform = GetComponent<RectTransform>();
-
-        // For Screen Space - Overlay canvas, uiCamera is null (that's fine)
-        Canvas canvas = GetComponentInParent<Canvas>();
-        uiCamera = canvas.worldCamera;
-    }
-
-    private void Start()
-    {
-        StartCoroutine(InitAfterLayout());
-    }
-
-    private IEnumerator InitAfterLayout()
-    {
-        // Wait two frames to be sure ContentSizeFitter has finished
-        yield return null;
-        yield return null;
-
-        float screenHeight = Screen.height;
-        float contentHeight = rectTransform.rect.height;
-
-        // Convert screen bottom and top to world space
-        Vector3 screenBottom = new Vector3(Screen.width / 2f, 0, 0);
-        Vector3 screenTop    = new Vector3(Screen.width / 2f, screenHeight, 0);
-
-        Vector3 worldBottom = uiCamera != null
-            ? uiCamera.ScreenToWorldPoint(screenBottom)
-            : Camera.main.ScreenToWorldPoint(screenBottom);
-
-        Vector3 worldTop = uiCamera != null
-            ? uiCamera.ScreenToWorldPoint(screenTop)
-            : Camera.main.ScreenToWorldPoint(screenTop);
-
-        // For Screen Space - Overlay, just use pixel positions directly
-        // Place content starting fully below the screen
-        Vector3 startPos = rectTransform.position;
-        startPos.y = worldBottom.y - contentHeight;
-        rectTransform.position = startPos;
-
-        // End when content is fully above the screen
-        endWorldY = worldTop.y + contentHeight;
-
-        canScroll = true;
-    }
+    [SerializeField] private Animator animacao;
 
     private bool isEnding = false;
 
-    private void Update()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            GoToMenu();
+            SceneManager.LoadScene(menuSceneNumber);
             return;
         }
 
-        if (!canScroll || isEnding) return;
+        if (isEnding) return;
 
-        Vector3 pos = rectTransform.position;
-        pos.y += scrollSpeed * Time.deltaTime;
-        rectTransform.position = pos;
+        transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
 
-        if (pos.y >= endWorldY)
+        if (transform.position.y >= endYPosition)
         {
             isEnding = true;
             animacao.Play("TransicaoSaida");
@@ -91,11 +35,6 @@ public class Credits : MonoBehaviour
     private IEnumerator VoltaProMenuComTempo()
     {
         yield return new WaitForSeconds(tempomenu);
-        GoToMenu();
-    }
-
-    private void GoToMenu()
-    {
         SceneManager.LoadScene(menuSceneNumber);
     }
 }
