34531f9fdbb2e7fd3d8746e48cd7d4a5b644ee04
Daniel <daniel.henriques.gamedev@gmail.com>
2026-05-31 15:34:26 +0100

    actually fixed the volume settings hopefully???

--- Assets/Scripts/KenScripts/VolumeSettings.cs
+++ Assets/Scripts/KenScripts/VolumeSettings.cs
@@ -3,12 +3,19 @@ using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] private Slider volumeSlider;
     [SerializeField] private float changeSpeed = 1f;
 
+    private Slider volumeSlider;
+
+    private void OnEnable()
+    {
+        volumeSlider = GetComponent<Slider>();
+    }
 
     private void Update()
     {
+        if (volumeSlider == null) return;
+
         float input = Input.GetAxis("Horizontal");
 
         if (input != 0)
